e2cc25aa6d8fbd6c1f2f3620074f22cf2c1eafc3
Kaitlynn Pham <kaitlynnpham@gmail.com>
2025-05-08 12:27:18 -0700

    update styling

--- loader.js
+++ loader.js
@@ -1,31 +1,30 @@
 
 window.onload = () => {
-    const welcomeScreen = document.getElementById('introduction');
-    const homepage = document.getElementById('homepage');
-  
-    const puzzles = [
-      document.getElementById('puzzle1'),
-      document.getElementById('puzzle2'),
-      document.getElementById('puzzle4'),
-      document.getElementById('puzzle3')
-      
-    ];
-  
-  
-    welcomeScreen.style.display = 'flex';
-  
-   
-   puzzles.forEach((tile, index) => {
-      setTimeout(() => {
-        tile.style.opacity = 1; 
-      }, index * 500); 
-    });
-  setTimeout(()=> {
+  const welcomeScreen = document.getElementById('introduction');
+  const homepage = document.getElementById('homepage');
+
+  const puzzles = [
+    document.getElementById('puzzle1'),
+    document.getElementById('puzzle2'),
+    document.getElementById('puzzle4'),
+    document.getElementById('puzzle3')
+
+  ];
+
+
+  welcomeScreen.style.display = 'flex';
+
+
+  puzzles.forEach((tile, index) => {
+    setTimeout(() => {
+      tile.style.opacity = 1;
+    }, index * 500);
+  });
+  setTimeout(() => {
 
     setTimeout(() => {
-      welcomeScreen.style.display = 'none';  
-      homepage.style.display = 'block';   
-    },1500);  
-    }, puzzles.length * 500); 
-  };
-  
+      welcomeScreen.style.display = 'none';
+      homepage.style.display = 'block';
+    }, 1500);
+  }, puzzles.length * 500);
+};
